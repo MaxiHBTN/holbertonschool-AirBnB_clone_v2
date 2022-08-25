f7bbf916f3976ad3e8f6b7dfdc57a6c22b6b2daf
MaxiHBTN <4387@holbertonschool.com>
2022-08-25 14:27:53 +0000

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,19 +22,20 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         r1 = run("mkdir -p /data/web_static/releases/\
             {}".format(filename_noext))
-        r2 = run("tar -xzf /tmp/{} -C /data/web_static/releases/\
-            {}/".format(filename, filename_noext))
+        r2 = run("tar -xzf /tmp/{} -C /data/web_static/\
+            releases/{}/".format(filename, filename_noext))
         r3 = run("rm /tmp/{}".format(filename))
-        r4 = run("mv /data/web_static/releases/{}/web_static/* \
+        r1 = run("mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/\
                 ".format(filename_noext, filename_noext))
-        r5 = run("rm -rf /data/web_static/releases/{}/\
-        web_static".format(filename_noext))
-        r6 = run("rm -rf /data/web_static/current")
-        r7 = run("ln -s /data/web_static/releases/{}/ \
+        r4 = run("rm -rf /data/web_static/releases/\
+            {}/web_static".format(filename_noext))
+        r5 = run("rm -rf /data/web_static/current")
+        r6 = run("ln -s /data/web_static/releases/{}/ \
             /data/web_static/current".format(filename_noext))
-        if (r1.failed or r2.failed or r3.failed or r4.failed
-                or r5.failed or r6.failed or r7.failed):
-            return False
+        if (r1.failed or r2.failed or r3.failed or
+                r4.failed or r5.failed or r6.failed):
+            return True
     else:
-        return True
+        return False
+  
