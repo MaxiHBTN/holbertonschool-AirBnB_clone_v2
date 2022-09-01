063f8b4ffdcd062382e9300431d73dc6e43b0bb6
MaxiHBTN <4387@holbertonschool.com>
2022-09-01 12:03:24 +0000

    Task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardon_appcontext(self):
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
